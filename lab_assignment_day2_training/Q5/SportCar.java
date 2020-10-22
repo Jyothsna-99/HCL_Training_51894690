a35ee5f27a1e8eee2ff583c75f64b27122699b4b
Vysyaraju Jyothsna <64741811+Jyothsna-99@users.noreply.github.com>
2020-10-22 12:43:47 +0530

    Add files via upload

---
+++ lab_assignment_day2_training/Q5/SportCar.java
@@ -0,0 +1,13 @@
+package lab_assignment_day2_training.Q5;
+
+public class SportCar extends Car {
+	public SportCar(int noOfWheel, int noOfPassenger, int model, String make, int noOfDoor) {
+		super(noOfWheel, noOfPassenger, model, make, noOfDoor);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void display() {
+		super.display();
+	}
+
+}
