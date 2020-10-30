63f4f615e77e62166b15b81accf113e54baa5fec
Vysyaraju Jyothsna <64741811+Jyothsna-99@users.noreply.github.com>
2020-10-30 18:11:01 +0530

    Update Test5.java

--- lab_assignment_day4_training/src/lab_assignment_day4_training/q5/Test5.java
+++ lab_assignment_day4_training/src/lab_assignment_day4_training/q5/Test5.java
@@ -3,16 +3,21 @@ package lab_assignment_day4_training.q5;
 import java.util.Scanner;
 
 public class Test5 {
-	public static void main(String[] args) {
+	public static void main(String[] args){
                 Scanner s= new Scanner(System.in);
-		try(s) {
+		try(s)
+		{
 			System.out.println("enter a number");
 			int n=s.nextInt();
-			try {
-				if(n>100) {
+			try
+			{
+				if(n>100)
+				{
 					throw new Exception();
 				}
-			}catch (Exception e) {
+			}
+			catch (Exception e)
+			{
 				System.out.println("Num not greater than 100");
 			}
 		}
