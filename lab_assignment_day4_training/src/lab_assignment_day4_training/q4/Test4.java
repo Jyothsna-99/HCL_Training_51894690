8dc95ceb558f3fd5924f02ce774dc3639a3b953e
Vysyaraju Jyothsna <64741811+Jyothsna-99@users.noreply.github.com>
2020-10-30 18:10:00 +0530

    Update Test4.java

--- lab_assignment_day4_training/src/lab_assignment_day4_training/q4/Test4.java
+++ lab_assignment_day4_training/src/lab_assignment_day4_training/q4/Test4.java
@@ -4,31 +4,36 @@ import java.util.Scanner;
 
 public class Test4 {
 
-	public static void main(String[] args) {
-		Scanner s= new Scanner(System.in);
+	public static void main(String[] args) 
+	{
 		int n = 0;
+		Scanner s= new Scanner(System.in);
 		try {
-
 			do {
 				System.out.println("enter -1 to quit");
 				num = s.nextInt();
 				evenOddNumCheck(n);
-			} while (n!= -1);
-			
-			
-		} catch (InputMismatchException e) {
+			   }
+		       	while (n!= -1);	
+		}
+		catch (InputMismatchException e)
+		{
 			System.out.println("enter an integer");
 		}
-		
 	}
-
-	private static void evenOddNumCheck(int n) {
-		if (n%2==0) {
-			System.out.println("even number");
-		} else if(n%2!=0){
+	       private static void evenOddNumCheck(int n)
+	       {
+		       if (n%2==0)
+		       {
+			        System.out.println("even number");
+		       }
+		       else if(n%2!=0)
+		       {
 			System.out.println("odd number");
-		} else {
+		       } 
+		       else 
+		       {
 			System.exit(0);
-		}
-	}
+		       }
+	      }
 }
