7e2c53c8440d2f03db4c87d7cee1426c93a7be99
Vysyaraju Jyothsna <64741811+Jyothsna-99@users.noreply.github.com>
2020-11-23 22:23:00 +0530

    Add files via upload

---
+++ BookApp/src/main/java/com/bookApp/controller/BookController.java
@@ -0,0 +1,36 @@
+package com.bookApp.controller;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.bookApp.persistance.Book;
+import com.bookApp.persistance.BookDao;
+import com.bookApp.persistance.BookDaoImpl;
+import com.bookApp.service.BookService;
+import com.bookApp.service.BookServiceImpl;
+
+public class BookController {
+
+	public static void main(String[] args) {
+		AbstractApplicationContext ctx= new ClassPathXmlApplicationContext("book.xml");
+		BookService dao=ctx.getBean("bookService",BookServiceImpl.class);
+		
+		System.out.println("....getting all books...");
+		dao.getAllBooks().forEach(System.out::println);
+		
+		
+		System.out.println("....getting book by id...");
+		System.out.println(dao.getBookById(9));
+		
+
+		Book bookUpdate=new Book("TMKA23","ADV JAVA","Moksha",8000);
+		dao.updateBook(4, bookUpdate);
+		
+		System.out.println("....getting all books after updating the book...");
+		dao.getAllBooks().forEach(System.out::println);
+		
+		
+		
+	}
+
+}
